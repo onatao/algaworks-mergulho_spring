f3026cafe796aa2e28281289e3d1a78194cb2b7c
Nathan Barros <nathanbarrosnt@gmail.com>
2023-05-17 13:30:23 -0300

    feature: base files

---
+++ src/test/java/com/devnatao/logapi/LogapiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnatao.logapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogapiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
